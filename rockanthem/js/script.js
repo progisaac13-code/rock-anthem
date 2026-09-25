34814a9ddb626f52a7807e57d7aae3e53251d488
Isaac Rocha <prog.isaac13@gmail.com>
2026-09-25 00:47:25 -0300

    ANTHEM

--- rockanthem/js/script.js
+++ rockanthem/js/script.js
@@ -48,80 +48,83 @@ const musicas = [
         audio: "audio/WHO I BECOME.mp3"
     }
 ];
-function play(i) {
+function play_music(i) {
     const audio = document.getElementById("audio");
     const playBtn = document.getElementById("playBtn");
     const progress = document.getElementById("progress");
     const currentTime = document.getElementById("currentTime");
     const duration = document.getElementById("duration");
     const volumeBtn = document.getElementById("volumeBtn");
-    const title = document.getElementById('music_title');
-    const img = document.getElementById('thumb_img')
-    
-    title.textContent = musicas[i].titulo;
-    img.src = musicas[i].capa;
-    audio.src = musicas[i].audio;
+
     playBtn.addEventListener("click", () => {
-    
         if (audio.paused) {
             audio.play();
             playBtn.textContent = "❚❚";
-    
         } else {
             audio.pause();
             playBtn.textContent = "▶";
         }
-    
     });
-    
+
     audio.addEventListener("loadedmetadata", () => {
         duration.textContent = formatTime(audio.duration);
     });
-    
+
     audio.addEventListener("timeupdate", () => {
-    
+
         const percentage =
             (audio.currentTime / audio.duration) * 100;
-    
+
         progress.value = percentage;
-    
+
         currentTime.textContent =
             formatTime(audio.currentTime);
-    
+
     });
-    
+
     progress.addEventListener("input", () => {
-    
+
         const time =
             (progress.value / 100) * audio.duration;
-    
+
         audio.currentTime = time;
-    
+
     });
-    
+
     audio.addEventListener("ended", () => {
         playBtn.textContent = "▶";
         progress.value = 0;
     });
-    
+
     volumeBtn.addEventListener("click", () => {
-    
+
         audio.muted = !audio.muted;
-    
+
         volumeBtn.textContent =
             audio.muted ? "🔇" : "🔊";
-    
+
     });
-    
+
     function formatTime(seconds) {
-    
+
         if (isNaN(seconds)) {
             return "0:00";
         }
-    
+
         const minutes = Math.floor(seconds / 60);
         const secs = Math.floor(seconds % 60);
-    
+
         return `${minutes}:${secs.toString().padStart(2, "0")}`;
     }
+}
+
+function play_(i) {
+    const title = document.getElementById('music_title');
+    const img = document.getElementById('thumb_img');
+    const audio = document.getElementById("audio");
+
+    title.textContent = musicas[i].titulo;
+    img.src = musicas[i].capa;
+    audio.src = musicas[i].audio;
+    play_music(i)
 }
